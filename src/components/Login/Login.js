3772b542191acab93d297e0b5e2ffcd0dd466ec7
jacklu97 <jacklu.hdz@gmail.com>
2019-08-13 12:25:43 -0500

    Login form 1st try

---
+++ src/components/Login/Login.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+import logo from '../../logoSF.png'
+import './Login.css'
+//BME820202JM6 RFC DEL CLIENTE 
+//PASS Bmercurio_131145
+
+
+class Login extends Component {
+    state= {
+        username: '',
+        password: '',
+        errors: []
+    }
+
+    comprobarIngreso = (e) =>{
+        e.preventDefault();
+        console.log(this.state)
+        if (this.state.username === "BME820202JM6" && this.state.password === "Bmercurio_131145"){
+            console.log("Exito")
+        }
+        else{
+            console.log("Tas todo meco")
+        }
+    }
+
+    onChangeHandler = (e) =>{
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    render() {
+        return (
+            <div className="container-fluid"> <div className="bg-image"></div>
+                <div className="container">
+                    <div id="login" className="signin-card">
+                    <div className="logo-image">
+                    <img src={logo} alt="Logo" title="Logo" width="138"/>
+                    </div>
+                    <h1 className="display1" style={{color: 'red'}}>Central <span style={{color: 'black'}}>Cargo</span></h1>
+                    
+                    <form>
+                        <div id="form-login-username" className="form-group">
+                            <input id="username" className="form-control" name="username" type="text" size="18" alt="login" required onChange = {this.onChangeHandler} />
+                            <span className="form-highlight"></span>
+                            <span className="form-bar"></span>
+                            <label className="float-label">Usuario</label>
+                        </div>
+                        <div id="form-login-password" className="form-group">
+                            <input id="passwd" className="form-control" name="password" type="password" size="18" alt="password" required onChange = {this.onChangeHandler}/>
+                            <span className="form-highlight"></span>
+                            <span className="form-bar"></span>
+                            <label className="float-label">Contraseña</label>
+                        </div>
+                        <div>
+                            <button disabled={this.state.username.length < 1 && this.state.password.length < 1 ? true : false} className="btn btn-block btn-info ripple-effect" type="submit" name="Submit" alt="sign in" onClick={this.comprobarIngreso}>Ingresar</button>  
+                        </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login
