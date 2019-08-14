8a6a90b5f922a251fa8b5ed4b74d7f23e1ca7539
jacklu97 <jacklu.hdz@gmail.com>
2019-08-14 17:35:45 -0500

    Factura Component created

---
+++ src/components/Facturas/FacturaComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FacturaComponent = (props) =>{
+    return (
+        <div>
+            Hola soy componente
+        </div>
+    )
+}
+
+export default FacturaComponent
